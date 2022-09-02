7c2fd516d4a8e2305414033da263cceb1c5ae5b0
PU6ER <64597307+PU6ER@users.noreply.github.com>
2022-09-02 19:33:07 +0300

    Add files via upload

---
+++ SHIRI/2/index.js
@@ -0,0 +1,153 @@
+
+
+ const htmlFragment = `<div class="test" style="display: flex;
+ flex-direction: column;
+ align-items: flex-start;
+ 
+ 
+ 
+ position: relative;
+ width: 607px;
+ height: 291px;
+ 
+ background: #FFFFFF;">
+     <p>Список товаров</p>
+     <div class="card_wrapper" style="display: flex;
+     flex-direction: row;
+     align-items: flex-start;
+     
+     gap: 16px;
+     
+     position: relative;
+     width: 607px;
+     height: 291px;
+     
+     background: #FFFFFF;"">
+     <div class="card" style="display: flex;
+     flex-direction: column;
+     align-items: center;
+     padding-top: 16px;
+     gap: 16px;
+     
+     position: relative;
+     width: 181px;
+     height: 221px;
+     
+     background: #F8F8F8;
+     border-radius: 24px;">
+         <div class="image" style="width: 149px;
+         height: 138px;
+         
+         background: #C4C4C4;
+         border-radius: 16px;
+         flex: none;
+ 
+         
+         "></div>
+         <div class="button" style="flex: none;
+         order: 1;
+         flex-grow: 0;"><button style="display: flex;
+     flex-direction: row;
+     align-items: center;
+     padding: 10px 54px;
+     gap: 10px;
+ 
+     position: relative;
+     width: 149px;
+     height: 35px;
+ 
+     background: #FFFFFF;
+     border-radius: 10px;
+     border-color: #F8F8F8;
+     border-width: 0;">Купить</button></div>
+     </div>
+     <div class="card" style="display: flex;
+     flex-direction: column;
+     align-items: center;
+     padding-top: 16px;
+     gap: 16px;
+     
+     position: relative;
+     width: 181px;
+     height: 221px;
+     
+     background: #F8F8F8;
+     border-radius: 24px;">
+         <div class="image" style="width: 149px;
+         height: 138px;
+         
+         background: #C4C4C4;
+         border-radius: 16px;
+         flex: none;
+ 
+         
+         "></div>
+         <div class="button" style="flex: none;
+         order: 1;
+         flex-grow: 0;"><button style="display: flex;
+     flex-direction: row;
+     align-items: center;
+     padding: 10px 54px;
+     gap: 10px;
+ 
+     position: relative;
+     width: 149px;
+     height: 35px;
+ 
+     background: #FFFFFF;
+     border-radius: 10px;
+     border-color: #F8F8F8;
+     border-width: 0;">Купить</button></div>
+     </div>
+     <div class="card" style="display: flex;
+     flex-direction: column;
+     align-items: center;
+     padding-top: 16px;
+     gap: 16px;
+     
+     position: relative;
+     width: 181px;
+     height: 221px;
+     
+     background: #F8F8F8;
+     border-radius: 24px;">
+         <div class="image" style="width: 149px;
+         height: 138px;
+         
+         background: #C4C4C4;
+         border-radius: 16px;
+         flex: none;
+ 
+         
+         "></div>
+         <div class="button" style="flex: none;
+         order: 1;
+         flex-grow: 0;"><button style="display: flex;
+     flex-direction: row;
+     align-items: center;
+     padding: 10px 54px;
+     gap: 10px;
+ 
+     position: relative;
+     width: 149px;
+     height: 35px;
+ 
+     background: #FFFFFF;
+     border-radius: 10px;
+     border-color: #F8F8F8;
+     border-width: 0;">Купить</button></div>
+     </div>
+ </div>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ </div>`;
+ 
+ module.exports = function () {
+     return htmlFragment;
+ };
+ 
